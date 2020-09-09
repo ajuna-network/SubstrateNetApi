c715a62e034e6b796da3b99c52a4d931775164c4
Cedric Decoster <cedric.decoster@gmail.com>
2020-09-10 00:36:40 +0200

    Ed25519 added added key derivation test

---
+++ SubstrateNetApi/SubstrateNetApiTest/Ed25519/SchnorrkelTest.cs
@@ -0,0 +1,45 @@
+using NUnit.Framework;
+using SubstrateNetApi;
+using System;
+using Chaos.NaCl;
+using System.Linq;
+using SubstrateNetApi.MetaDataModel.Values;
+
+namespace SubstrateNetApiTests.ClientTests
+{
+    public class Ed25519Test
+    {
+        private Random _random;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _random = new Random();
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+        }
+
+        [Test]
+        public void Ed25519KeyPairTest()
+        {
+            string priKey0x = "0xf5e5767cf153319517630f226876b86c8160cc583bc013744c6bf255f5cc0ee5278117fc144c72340f67d0f2316e8386ceffbf2b2428c9c51fef7c597f1d426e";
+            string pubKey0x = "0x278117fc144c72340f67d0f2316e8386ceffbf2b2428c9c51fef7c597f1d426e";
+
+            byte[] priKey = Utils.HexToByteArray(priKey0x);
+            byte[] pubKey = Utils.HexToByteArray(pubKey0x);
+            byte[] seed = priKey.Take(32).ToArray();
+
+            Ed25519.KeyPairFromSeed(out pubKey, out priKey, seed);
+
+            Assert.AreEqual("0xF5E5767CF153319517630F226876B86C8160CC583BC013744C6BF255F5CC0EE5", Utils.Bytes2HexString(seed));
+            Assert.AreEqual("0xF5E5767CF153319517630F226876B86C8160CC583BC013744C6BF255F5CC0EE5278117FC144C72340F67D0F2316E8386CEFFBF2B2428C9C51FEF7C597F1D426E", Utils.Bytes2HexString(priKey));
+            Assert.AreEqual("0x278117FC144C72340F67D0F2316E8386CEFFBF2B2428C9C51FEF7C597F1D426E", Utils.Bytes2HexString(pubKey));
+
+            var account = new AccountId("0x278117FC144C72340F67D0F2316E8386CEFFBF2B2428C9C51FEF7C597F1D426E");
+            Assert.AreEqual("5CxW5DWQDpXi4cpACd62wzbPjbYrx4y67TZEmRXBcvmDTNaM", account.Address);
+        }
+    }
+}
--- SubstrateNetApi/SubstrateNetApiTest/Schnorrkel/SchnorrkelTest.cs
+++ SubstrateNetApi/SubstrateNetApiTest/Schnorrkel/SchnorrkelTest.cs
@@ -6,7 +6,9 @@ using System;
 using System.Threading.Tasks;
 using SubstrateNetApi.Exceptions;
 using Schnorrkel;
+using Schnorrkel.Merlin;
 using System.Text;
+using Schnorrkel.Signed;
 
 namespace SubstrateNetApiTests.ClientTests
 {
@@ -26,8 +28,31 @@ namespace SubstrateNetApiTests.ClientTests
         }
 
         [Test]
-        public void SecretKeyPublicKeyTest()
+        public void ShouldGenerateKeypair11Test()
         {
+            //var seed0x = "0xa81056d713af1ff17b599e60d287952e89301b5208324a0529b62dc7369c745defc9c8dd67b7c59b201bc164163a8978d40010c22743db142a47f2e064480d4b";
+
+            //byte[] seed = Utils.HexToByteArray(seed0x);
+
+            //var secretKey = SecretKey.FromBytes011(seed);
+
+            //Assert.AreEqual("0x15C2EA7AE2F5237E2FCB134CFAB0D2251166430A4146A920C5B6E5D88693AE0B", Utils.Bytes2HexString(secretKey.key.GetBytes()));
+            //Assert.AreEqual("0xEFC9C8DD67B7C59B201BC164163A8978D40010C22743DB142A47F2E064480D4B", Utils.Bytes2HexString(secretKey.nonce));
+        }
+
+
+        [Test]
+        public void ShouldGenerateKeypair85Test()
+        {
+            //var seed0x = "0xa81056d713af1ff17b599e60d287952e89301b5208324a0529b62dc7369c745defc9c8dd67b7c59b201bc164163a8978d40010c22743db142a47f2e064480d4b";
+
+            //byte[] seed = Utils.HexToByteArray(seed0x);
+
+            //var secretKey = SecretKey.FromBytes085(seed);
+            //var pubkey1 = Utils.Bytes2HexString(secretKey.nonce);
+
+            //Assert.AreEqual("0xA81056D713AF1FF17B599E60D287952E89301B5208324A0529B62DC7369C745D", Utils.Bytes2HexString(secretKey.key.GetBytes()));
+            //Assert.AreEqual("0xEFC9C8DD67B7C59B201BC164163A8978D40010C22743DB142A47F2E064480D4B", Utils.Bytes2HexString(secretKey.nonce));
         }
 
 
