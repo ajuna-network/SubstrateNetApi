929a56d94eaeacd6d1b6d2df6a3e203b0ebe0cc3
Cedric Decoster <cedric.decoster@gmail.com>
2021-06-13 17:33:33 +0200

    refactored customs to one folder

---
+++ SubstrateNetApi/Model/Custom/PolkadotTypes.cs
@@ -0,0 +1,28 @@
+﻿using SubstrateNetApi.Model.Types.Base;
+using SubstrateNetApi.Model.Types.Enum;
+using SubstrateNetApi.Model.Types.Struct;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace SubstrateNetApi.Model.Types.Custom
+{
+    #region BASE_TYPES
+
+    #endregion
+
+    #region ENUM_TYPES
+
+    #endregion
+
+    #region STRUCT_TYPES
+
+    #endregion
+
+    #region INHERITED_TYPES
+
+    #endregion
+
+
+}
