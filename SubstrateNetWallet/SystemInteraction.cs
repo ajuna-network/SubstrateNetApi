87556d0b3392b4c2e0409124601512e569896116
Cedric Decoster <cedric.decoster@gmail.com>
2021-07-31 13:55:52 +0200

    updated documentation systeminteraction

--- SubstrateNetWallet/SystemInteraction.cs
+++ SubstrateNetWallet/SystemInteraction.cs
@@ -5,12 +5,52 @@ namespace SubstrateNetWallet
 {
     public static class SystemInteraction
     {
+        /// <summary>
+        /// Gets or sets the read data.
+        /// </summary>
+        /// <value>
+        /// The read data.
+        /// </value>
         public static Func<string, string> ReadData { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the data exists.
+        /// </summary>
+        /// <value>
+        /// The data exists.
+        /// </value>
         public static Func<string, bool> DataExists { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the read persistent.
+        /// </summary>
+        /// <value>
+        /// The read persistent.
+        /// </value>
         public static Func<string, string> ReadPersistent { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the persistent exists.
+        /// </summary>
+        /// <value>
+        /// The persistent exists.
+        /// </value>
         public static Func<string, bool> PersistentExists { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the persist.
+        /// </summary>
+        /// <value>
+        /// The persist.
+        /// </value>
         public static Action<string, string> Persist { get; set; }
-
+        
+        /// <summary>
+        /// Reads all text.
+        /// </summary>
+        /// <param name="path">The path.</param>
+        /// <returns></returns>
+        /// <exception cref="FileNotFoundException"></exception>
         public static string ReadAllText(string path)
         {
             // First check if file exists in persistent store.
